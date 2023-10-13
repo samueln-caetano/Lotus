fb84212758a12738cf2d7ce687dcc97bd4434be6
Samuel Nogueira <124512841+samueln-caetano@users.noreply.github.com>
2023-10-13 19:30:32 -0300

    Frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Signup from './Signup';
+import Login from './Login';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/register' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
